99afdc96d2c4daa623b0427f7cd67ad4ef320eac
Delly <60603701+UkiDelly@users.noreply.github.com>
2025-04-17 07:30:33 +0900

    Add: pnpm workspace configuration and update Genre entity formatting

--- src/genre/entities/genre.entity.ts
+++ src/genre/entities/genre.entity.ts
@@ -1,15 +1,15 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { BaseTable } from '../../common/entity/base.entity';
-import { Movie } from '../../movies/entities/movie.entity';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { BaseTable } from '../../common/entity/base.entity'
+import { Movie } from '../../movies/entities/movie.entity'
 
 @Entity()
 export class Genre extends BaseTable {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
   @Column({ unique: true })
-  name: string;
+  name: string
 
   @ManyToMany(() => Movie, movie => movie.genres)
-  movies: Movie[];
+  movies: Movie[]
 }
