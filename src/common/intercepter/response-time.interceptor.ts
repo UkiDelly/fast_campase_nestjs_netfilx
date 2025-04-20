0cfec7aa6997ef1e1a96b720269d1ae150f1dda7
Delly <60603701+UkiDelly@users.noreply.github.com>
2025-04-20 09:50:29 +0900

    Update: 응답 시간 인터셉터에 타임아웃 처리 추가
    - 응답 시간이 1초를 초과할 경우 InternalServerErrorException을 던지도록 수정
    - 응답 시간 측정을 위해 1초 지연 추가

--- src/common/intercepter/response-time.interceptor.ts
+++ src/common/intercepter/response-time.interceptor.ts
@@ -2,11 +2,11 @@
 https://docs.nestjs.com/interceptors#interceptors
 */
 
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { CallHandler, ExecutionContext, Injectable, InternalServerErrorException, NestInterceptor } from '@nestjs/common'
 import dayjs from 'dayjs'
 import type { Request } from 'express'
 import { Observable } from 'rxjs'
-import { tap } from 'rxjs/operators'
+import { delay, tap } from 'rxjs/operators'
 
 @Injectable()
 export class ResponseTimeInterceptor implements NestInterceptor {
@@ -15,9 +15,17 @@ export class ResponseTimeInterceptor implements NestInterceptor {
     const start = dayjs()
 
     return next.handle().pipe(
+      delay(1000),
       tap(() => {
         const responseTime = dayjs()
         const diff = responseTime.diff(start, 'ms')
+
+        // 에러를 던질수도 있음
+        // 1초 이상 걸린다면 TIME OUT 처리
+        if (diff > 1000) {
+          console.warn(`[${req.method}] - ${req.url} - TIME OUT!!`)
+          throw new InternalServerErrorException('서버 처리 시간이 너무 오래 걸렸습니다.')
+        }
 
         console.log(`[${req.method}] - ${req.url} - ${diff}ms`)
       })
